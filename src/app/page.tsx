19ebae8973d93d697440ddfc3c65fa00de55687b
Cameron Greene <cam.greene@gmail.com>
2024-12-27 22:14:06 -0800

    WORK ON HOME PAGE

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,108 @@
-import Image from "next/image";
+'use client'
+import { useEffect, useRef } from 'react'
 
-export default function Home() {
+interface Moon {
+  angularVelocity: number
+  radius: number
+  positionRadians: number
+}
+
+interface CarrierRing {
+  centerX: number
+  centerY: number
+  radius: number
+  moons: Moon[]
+  draw(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D, elapsed: DOMHighResTimeStamp): void
+}
+
+const createCarrierRing = (centerX: number, centerY: number, radius: number, moons: Moon[]): CarrierRing => {
+  return {
+    centerX,
+    centerY,
+    radius,
+    moons,
+    draw(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D, elapsed: DOMHighResTimeStamp) {
+
+      console.log(elapsed)
+
+      ctx.beginPath()
+      ctx.arc(this.centerX, this.centerY, this.radius, 0, Math.PI)
+      ctx.strokeStyle = '#ffffff'
+      ctx.stroke()
+      ctx.closePath()
+
+      for (const moon of this.moons) {
+        const thisX: number = this.centerX + Math.cos(moon.positionRadians) * this.radius
+        const thisY: number = this.centerY + Math.sin(moon.positionRadians) * this.radius
+
+        ctx.beginPath()
+        ctx.arc(thisX, thisY, moon.radius, 0, Math.PI * 2)
+        ctx.strokeStyle = '#ffffff'
+        ctx.stroke()
+        ctx.closePath()
+
+        // moon.positionRadians += moon.angularVelocity * elapsed
+      }
+    }
+  }
+}
+
+export default function Page() {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null)
+
+  useEffect(() => {
+    document.title = 'greene.cam'
+
+    const canvas = canvasRef.current
+    if (!canvas) return
+
+    canvas.width = document.documentElement.clientWidth;
+    canvas.height = document.documentElement.clientHeight;
+
+    const ctx = canvas.getContext('2d')
+    if (!ctx) return
+
+    let animationFrameId: number
+    let previousTimestamp: DOMHighResTimeStamp
+    const MAX_DELTA_TIME: number = 0.05;
+
+    const circles: Moon[] = [
+      {angularVelocity: 0.1, radius: 20 , positionRadians: Math.PI / 2},
+      {angularVelocity: -0.3, radius: 30 , positionRadians: 3 * Math.PI / 2}
+    ]
+
+    const carrierRing = createCarrierRing(canvas.width / 2, canvas.height / 2, 200, circles)
+
+    const animateStep = (timestamp: DOMHighResTimeStamp) => {
+      if (previousTimestamp === undefined) {
+        previousTimestamp = timestamp
+      }
+
+      let elapsed: DOMHighResTimeStamp = (timestamp - previousTimestamp) / 1000
+      previousTimestamp = timestamp
+
+      elapsed = Math.min(elapsed, MAX_DELTA_TIME);
+
+      ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+      carrierRing.draw(canvas, ctx, elapsed)
+
+      animationFrameId = window.requestAnimationFrame(animateStep)
+    }
+
+    window.requestAnimationFrame(animateStep)
+    return () => window.cancelAnimationFrame(animationFrameId)
+  }, [])
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    <>
+      <canvas
+          ref={canvasRef}
+          style={{
+            border: '1px solid black',
+            display: 'block',
+          }}/>
+      <div className="bg-blue-500 w-20 h-20"></div>
+    </>
+  )
 }
